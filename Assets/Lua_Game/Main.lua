a7b45467cb825baf3d4efc23c37ff74998465a45
hbszqf <523184236@qq.com>
2018-07-10 09:44:36 +0800

    tolua 部分

---
+++ Assets/Lua_Game/Main.lua
@@ -0,0 +1,18 @@
+
+-- local print = print
+-- function __G__TRACKBACK__(errorMessage)
+--     print("----------------------------------------")
+--     print("[LUA ERROR]: "..tostring(errorMessage).."\n")
+--     print(debug.traceback("", 2))
+--     print("----------------------------------------")
+    
+-- end
+
+
+print("hello")
+-- xpcall( function()
+--     local num1  = 1
+--     local num2  = 2
+--     local sum = num1+num2
+--     print("sum==="..sum)
+-- end, __G__TRACKBACK__)
---
+++ Assets/Script/Main.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LuaInterface;
+
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Start");
+
+        LuaState lua = new LuaState();
+        lua.Start();
+        lua.AddSearchPath("D:/learn/UnityFrameWork/Assets/Lua_Game");
+     
+        lua.DoFile("Main.lua");
+
+    }
+
+    private void Awake()
+    {
+        Debug.Log("Awake");
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //Debug.Log("Update");
+    }
+
+
+}
+
+
+/// window lua path
